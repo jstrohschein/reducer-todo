c1d5b44064ededa2d01ba2c81ab35aa64a28e9b3
Jared Strohschein <45718292+jstrohschein@users.noreply.github.com>
2020-09-17 23:30:24 -0400

    change and push

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,29 +1,54 @@
-import React, { useReducer } from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
-import TodoForm from './components/TodoForm'
-import Todo from './components/Todo'
-import {todoReducer, ADD_TODO, TOGGLE_COMPLETED, initialTodoState} from './reducers/todoReducer'
+
+
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add-todo':
+      return {todos: [...state.todos, { item: action.payload, completed: false, id: new Date()}] };
+    case 'toggle-todo':
+      return {
+        todos: state.todos.map((task, index) => index === action.payload ? {...task, completed: !task.completed} : task)
+      }
+    default:
+      return state;
+  }
+}
 
 function App() {
 
-  const [state, dispatch] = useReducer(todoReducer, initialTodoState)
-
-  const addNewTask = newTask => {
-    
-    dispatch({ type: ADD_TODO, payload: newTask})
-
-  }
-
-
-
-
+  const [{ todos }, dispatch] = useReducer(reducer, { todos: [] })
+  const [text, setText] = useState()
 
 
   return (
     <div className="App">
-      <TodoForm addNewTask={addNewTask}/>
-      <Todo tasks={state}/>
-    </div>
+      <form onSubmit = {e => {
+        e.preventDefault()
+        dispatch({ type: 'add-todo', payload: text })
+        setText('');
+      }}>
+
+        <input value={text} onChange={e => setText(e.target.value)} />
+      </form>
+    
+      {todos.map((task, index) => (
+        <div 
+        key={task.id} 
+        onClick={() => dispatch({type: 'toggle-todo', payload: index})}
+        style={{
+          textDecoration: task.completed ? 'line-through' : ''
+        }}
+        > {task.item} 
+        </div>
+      ))}
+
+
+{/*   <pre>{JSON.stringify(todos, null, 2)}</pre>*/}    
+
+</div>
   );
 }
 
